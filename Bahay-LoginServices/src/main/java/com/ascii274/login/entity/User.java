f5c0b0649118ad59724487a40fda36ebb3d29939
Joel Cabatay <ascii274@gmail.com>
2023-08-23 00:06:57 +0200

    JpaConfiguration added

--- Bahay-LoginServices/src/main/java/com/ascii274/login/entity/User.java
+++ Bahay-LoginServices/src/main/java/com/ascii274/login/entity/User.java
@@ -1,5 +1,6 @@
 package com.ascii274.login.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -7,26 +8,38 @@ import lombok.*;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 @Builder
+@Table(name = "users")
 public class User {
 
     public User(String userName){
         this.userName = userName;
     }
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+
+    @Column(name="id")
     private Long id;
 
+    @Column(name="user_id")
     private String userId;
 
+    @Column(name="password")
     private String password;
 
+    @Column(name="user_name")
     private String userName;
 
 
+    @Column(name="first_name")
     private String firstName;
 
+    @Column(name="last_name")
     private String lastName;
 
+    @Column(name="mail")
     private String mail;
 
 
