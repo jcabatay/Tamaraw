44311700c851898f54e8bd858cb3694b653ed00e
Joel Cabatay <ascii274@gmail.com>
2024-07-04 11:31:07 +0200

    Update test, cleaning code

--- Tamaraw-LoginServices/src/test/java/com/ascii274/TamarawLoginServicesAppTest.java
+++ Tamaraw-LoginServices/src/test/java/com/ascii274/TamarawLoginServicesAppTest.java
@@ -1,44 +1,15 @@
 package com.ascii274;
 
-import com.ascii274.login.controller.UserController;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@SpringBootTest
-@SpringBootTest(classes = {TamarawLoginServicesAppTest.class})
-@AutoConfigureMockMvc
-
+@SpringBootTest
 class TamarawLoginServicesAppTest {
 
-	@Autowired
-	private UserController userController;
-
-	@Autowired
-	private MockMvc mvc;
 
 	@Test
-	public void shouldReturnDefaultMessage() throws Exception{
-
-		this.mvc.perform( get ("/v1/test-message"))
-				.andDo(print())
-				.andExpect(status()
-						.isOk())
-				.andExpect(content().string(containsString("Hello, Tamaraw Login Services")));
+	void contextLoads() {
 	}
-
-	@Test
-	void contextLoads() throws Exception{
-		assertThat(userController).isNotNull();
-	}
-
 }
